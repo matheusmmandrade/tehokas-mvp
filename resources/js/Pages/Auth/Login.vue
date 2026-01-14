effab619e08feabeccbeb39955d45db34e041d48
Matheus <matheusmmandrade@gmail.com>
2026-01-14 19:25:23 -0300

    fix: correct typo in forgot password translation

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -84,7 +84,7 @@ const submit = () => {
                     :href="route('password.request')"
                     class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                     Esqueceu sua senha??
+                     Esqueceu sua senha?
                 </Link>
 
                 <PrimaryButton
